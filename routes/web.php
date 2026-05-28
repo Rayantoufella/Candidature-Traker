9c64e7b516746790863064b1d237c1a64ec92abf
Rayan Toufella <rayantoufella3@gmail.com>
2026-05-28 15:03:59 +0100

    fix: restructurer les routes entretien en routes imbriquées sous candidatures

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,27 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware('auth')->group(function () {
+    Route::get('/candidatures', [\App\Http\Controllers\CandidatureController::class, 'index'])->name('candidatures.index');
+    Route::get('/candidatures/create', [\App\Http\Controllers\CandidatureController::class, 'create'])->name('candidatures.create');
+    Route::post('/candidatures', [\App\Http\Controllers\CandidatureController::class, 'store'])->name('candidatures.store');
+    Route::get('/candidatures/{candidature}', [\App\Http\Controllers\CandidatureController::class, 'show'])->name('candidatures.show');
+    Route::get('/candidatures/{candidature}/edit', [\App\Http\Controllers\CandidatureController::class, 'edit'])->name('candidatures.edit');
+    Route::put('/candidatures/{candidature}', [\App\Http\Controllers\CandidatureController::class, 'update'])->name('candidatures.update');
+    Route::delete('/candidatures/{candidature}', [\App\Http\Controllers\CandidatureController::class, 'destroy'])->name('candidatures.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/candidatures/{candidature}/entretiens', [\App\Http\Controllers\EntretienController::class, 'index'])->name('candidatures.entretiens.index');
+    Route::get('/candidatures/{candidature}/entretiens/create', [\App\Http\Controllers\EntretienController::class, 'create'])->name('candidatures.entretiens.create');
+    Route::post('/candidatures/{candidature}/entretiens', [\App\Http\Controllers\EntretienController::class, 'store'])->name('candidatures.entretiens.store');
+    Route::get('/candidatures/{candidature}/entretiens/{entretien}', [\App\Http\Controllers\EntretienController::class, 'show'])->name('candidatures.entretiens.show');
+    Route::get('/candidatures/{candidature}/entretiens/{entretien}/edit', [\App\Http\Controllers\EntretienController::class, 'edit'])->name('candidatures.entretiens.edit');
+    Route::put('/candidatures/{candidature}/entretiens/{entretien}', [\App\Http\Controllers\EntretienController::class, 'update'])->name('candidatures.entretiens.update');
+    Route::delete('/candidatures/{candidature}/entretiens/{entretien}', [\App\Http\Controllers\EntretienController::class, 'destroy'])->name('candidatures.entretiens.destroy');
+    Route::patch('/candidatures/{candidature}/entretiens/{entretien}/archiver', [\App\Http\Controllers\EntretienController::class, 'archiver'])->name('candidatures.entretiens.archiver');
+    Route::patch('/candidatures/{candidature}/entretiens/{entretien}/restore', [\App\Http\Controllers\EntretienController::class, 'restore'])->name('candidatures.entretiens.restore');
+    Route::delete('/candidatures/{candidature}/entretiens/{entretien}/force-delete', [\App\Http\Controllers\EntretienController::class, 'forceDelete'])->name('candidatures.entretiens.force-delete');
+});
+
 require __DIR__.'/auth.php';
